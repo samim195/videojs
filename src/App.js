a3fbafece4c6aecd860c6dce85af165c405eb81f
samimkohistani <samimkohistani@hotmail.com>
2021-01-23 16:32:18 +0000

    default big buck bunny

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
 
     this.player = {}
     this.state = {
-        video: "",
+        video: "https://multiplatform-f.akamaihd.net/i/multi/will/bunny/big_buck_bunny_,640x360_400,640x360_700,640x360_1000,950x540_1500,.f4v.csmil/master.m3u8",
         title: "Video JS"
     }
   }
